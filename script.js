216a4d68cbcef63a1ed24cb05ba30b027333b345
an-agha <84777499+an-agha@users.noreply.github.com>
2021-06-19 14:48:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// select elements from html
+time = document.getElementsByTagName('h1');
+hour = time[0];
+minute = time[1];
+second = time[2];
+
+setInterval(function get_time()
+{
+    date = Date();
+    get_hour = date.slice(16, 18);
+    // convert 24 hour to 12
+    if (get_hour > 12)
+        get_hour -= 12;
+    // covert 1 digit to 2 digit
+    if (get_hour < 10)
+        get_hour = '0' + get_hour;
+    hour.innerText = get_hour;
+    minute.innerText = date.slice(19, 21);
+    second.innerText = date.slice(22, 24);
+})
